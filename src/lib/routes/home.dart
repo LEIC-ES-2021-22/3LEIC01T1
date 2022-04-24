8aac3c37e240723c832e059d5a09d8e1b4fdef49
Vasco <vsngarcia5@gmail.com>
2022-04-24 20:39:11 +0100

    Firestore database initial setup Add screen to list courses

--- src/lib/routes/home.dart
+++ src/lib/routes/home.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:remind_me_up/event_card.dart';
 import 'package:remind_me_up/models/event.dart';
 import 'package:remind_me_up/routes/create_event.dart';
+import 'package:remind_me_up/routes/courses.dart';
 import 'package:remind_me_up/services/auth.dart';
 
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
-
-  final AuthService _auth = AuthService();
 
   final List<Event> events = [
     Event(
@@ -47,97 +46,152 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children:[
-            DrawerHeader(
-              child: Text('RemindMeUP')
-              ),
-            ListTile(
-              leading: const Icon(Icons.add_circle),
-              title: const Text("Create Event"),
-              onTap: (){
-                print("clicked on create event");
-                Navigator.push(
-                  context, MaterialPageRoute(builder: (context)=> CreateEvent())
-                );
-
-              },
-            )
-          ]
+    return DefaultScaffold(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 4),
+                  child: Text(
+                    'ALL EVENTS',
+                    style: TextStyle(
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ...events.map((e) => EventCard(event: e)).toList(),
+          ],
         ),
-        
       ),
+    );
+  }
+}
+
+class DefaultScaffold extends StatelessWidget {
+  const DefaultScaffold({Key? key, required this.child}) : super(key: key);
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const DefaultDrawer(),
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            floating: true,
-            actions: [
-              ElevatedButton(
-                onPressed: () => showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return SimpleDialog(
-                        titleTextStyle: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        title: Text(_auth.user?.displayName ??
-                            _auth.user?.email ??
-                            'User'),
-                        children: <Widget>[
-                          SimpleDialogOption(
-                            onPressed: () {
-                              _auth.logout();
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Sign Out'),
-                          ),
-                        ],
-                      );
-                    }),
-                style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(),
-                  minimumSize: Size.zero,
-                  padding: const EdgeInsets.all(2),
-                ),
-                child: const CircleAvatar(
-                  backgroundImage: AssetImage('assets/okayeg.png'),
-                ),
-              ),
-              const SizedBox(width: 15)
-            ],
-          ),
+          DefaultAppBar(),
           SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Row(
-                    children: const [
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 4),
-                        child: Text(
-                          'ALL EVENTS',
-                          style: TextStyle(
-                            letterSpacing: 3,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  ...events.map((e) => EventCard(event: e)).toList(),
-                ],
+            child: child,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DefaultDrawer extends StatelessWidget {
+  const DefaultDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const SizedBox(
+            height: 100,
+            child: DrawerHeader(
+              child: Text(
+                'RemindMe UP',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -2,
+                  color: Colors.deepPurple,
+                ),
               ),
             ),
           ),
+          ListTile(
+            title: const Text('Home'),
+            onTap: () => Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Home())),
+          ),
+          ListTile(
+            title: const Text('Courses'),
+            onTap: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const CoursesScreen())),
+          ),
+          ListTile(
+            leading: const Icon(Icons.add_circle),
+            title: const Text("Create Event"),
+            onTap: (){
+              print("clicked on create event");
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context)=> CreateEvent())
+              );
+
+            },
+          )
         ],
       ),
+    );
+  }
+}
+
+class DefaultAppBar extends StatelessWidget {
+  DefaultAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      floating: true,
+      actions: [
+        ElevatedButton(
+          onPressed: () => showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return SimpleDialog(
+                  titleTextStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  title: Text(
+                      _auth.user?.displayName ?? _auth.user?.email ?? 'User'),
+                  children: <Widget>[
+                    SimpleDialogOption(
+                      onPressed: () {
+                        _auth.logout();
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Sign Out'),
+                    ),
+                  ],
+                );
+              }),
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            minimumSize: Size.zero,
+            padding: const EdgeInsets.all(2),
+          ),
+          child: const CircleAvatar(
+            backgroundImage: AssetImage('assets/okayeg.png'),
+          ),
+        ),
+        const SizedBox(width: 15)
+      ],
     );
   }
 }
