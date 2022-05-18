707fcd03f065985dbae6298eabc23520419ff0b6
Vasco <vsngarcia5@gmail.com>
2022-05-18 10:08:40 +0100

    Make FlooredDuration parse absolute Duration Add quiver package Override equality operator and hashCode in FlooredDuration

--- src/lib/util.dart
+++ src/lib/util.dart
@@ -1,6 +1,7 @@
 import 'package:duration/duration.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:quiver/core.dart';
 
 class FlooredDuration {
   DurationTersity tersity;
@@ -10,7 +11,8 @@ class FlooredDuration {
 
   static FlooredDuration fromDuration(Duration dur) {
     for (final t in DurationTersity.list) {
-      var n = dur.inUnit(t);
+      var n = dur.inUnit(t).abs();
+
       // stop at seconds
       if (n > 0 || t == DurationTersity.second) {
         return FlooredDuration(t, n);
@@ -20,21 +22,27 @@ class FlooredDuration {
     throw Never;
   }
 
-
   String pluralizedTersity() {
     return Intl.plural(amount, one: tersity.name, other: tersity.name + 's');
   }
 
   String formatted() {
-    return '$amount ${tersity.name[0].toUpperCase()}${pluralizedTersity()
-        .substring(1)
-        .toLowerCase()}';
+    return '$amount ${tersity.name[0].toUpperCase()}${pluralizedTersity().substring(1).toLowerCase()}';
   }
 
   @override
   String toString() {
     return formatted();
   }
+
+  @override
+  bool operator ==(Object other) =>
+      other is FlooredDuration &&
+      other.tersity == tersity &&
+      other.amount == amount;
+
+  @override
+  int get hashCode => hash2(tersity, amount);
 }
 
 const fixedInputDecoration = InputDecoration(
