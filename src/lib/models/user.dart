2a45fd1493e564cd0d4895f65b32960e2c3bf115
Fab <fabiomiguelchenhuang@gmail.com>
2022-05-25 18:17:40 +0100

    the error here

--- src/lib/models/user.dart
+++ src/lib/models/user.dart
@@ -1,17 +1,22 @@
+import 'dart:ffi';
+
 class UserData {
   // final String uid;
   final List<String> courses;
+  final String? notificationToken;
 
-  UserData({required this.courses});
+  UserData({required this.courses, this.notificationToken});
 
   UserData.fromJson(Map<String, Object?> json)
       : this(
           courses: (json['courses']! as List).map((e) => e as String).toList(),
+          notificationToken: json['notificationToken'] as String,
         );
 
   Map<String, Object?> toJson() {
     return {
       'courses': courses,
+      'notificationTokens': notificationToken,
     };
   }
 }
