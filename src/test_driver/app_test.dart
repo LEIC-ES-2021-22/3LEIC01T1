6cb4b9595d07dc476b5465a191bc72957e2cbec5
Vasco <vsngarcia5@gmail.com>
2022-05-26 16:10:55 +0100

    Gherkin tests still not working...

--- src/test_driver/app_test.dart
+++ src/test_driver/app_test.dart
@@ -1,20 +1,21 @@
 import 'dart:async';
-import 'package:gherkin/gherkin.dart';
+
 import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:gherkin/gherkin.dart';
+import 'package:quiver/pattern.dart';
 
 import 'steps/type_in.dart';
 import 'steps/users_step.dart';
 
 Future<void> main() {
   final config = FlutterTestConfiguration()
-    ..features = [RegExp('features/*.*.feature')]
-    ..reporters = [
-      ProgressReporter(),
-      TestRunSummaryReporter(),
-      // JsonReporter(path: './report.json')
-    ]
-    ..stepDefinitions = [UsersStep(), TypeInStep()]
+    ..features = [Glob(r"test_driver/features/**.feature")]
+    ..reporters = [ProgressReporter(), TestRunSummaryReporter()]
+    ..stepDefinitions = [TypeInStep(), UsersStep()]
+    ..logFlutterProcessOutput = true
+    ..verboseFlutterProcessLogs = true
     ..restartAppBetweenScenarios = true
+    // ..targetAppWorkingDirectory = '../'
     ..targetAppPath = "test_driver/app.dart";
 
   return GherkinRunner().execute(config);
--- src/test_driver/steps/users_step.dart
+++ src/test_driver/steps/users_step.dart
@@ -1,6 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 
 StepDefinitionGeneric UsersStep() {
@@ -8,8 +5,9 @@ StepDefinitionGeneric UsersStep() {
     for (var row in table.rows) {
       var cols = row.columns.where((s) => s != null).map((s) => s!.trim()).toList();
       if (cols.length == 2) {
-        FirebaseAuth.instance
-            .createUserWithEmailAndPassword(email: cols[0].trim(), password: cols[1].trim());
+        print(cols[0] + ' ' + cols[1]);
+        // FirebaseAuth.instance
+        //     .createUserWithEmailAndPassword(email: cols[0].trim(), password: cols[1].trim());
       }
     }
   });
